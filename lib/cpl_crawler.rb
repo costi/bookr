a2a5fc7b2d86e8ad4f11f2ad3f497f0c32bf121c
Constantin Gavrilescu <costi@costi-thinkpad.(none)>
2009-10-27 22:18:29 -0500

    Tests for crawler

--- lib/cpl_crawler.rb
+++ lib/cpl_crawler.rb
@@ -3,28 +3,42 @@ class CplCrawler
   def initialize(card_number, zip_code)
     @card_number = card_number
     @zip_code = zip_code
+    @summary_page = nil
+    @agent = WWW::Mechanize.new { |agent|
+      agent.user_agent_alias = 'Mac Safari'
+    }
   end
 
   def login_to_library
-    @current_page = WWW::Mechanize.new.post('http://www.chipublib.org/mycpl/login//mycpl/login/', {:card_number => @card_number, :zip_code => @zip_code})
-    self.html = page.body    
-
+    @agent.get('http://www.chipublib.org/mycpl/login/') do |login_page|
+      @homepage = login_page.form_with(:action => '/mycpl/login/') do |f|
+        f.patronId = @card_number
+        f.zipCode = @zip_code
+      end.click_button
+    end
+    @homepage
   end
 
   def holds_page
-    
+    summary_page  
   end
   
   def checked_out_page
-    
+    summary_page
   end
 
   def overdue_page
-    
+    summary_page
   end
   
   def fines_page
-    
+    summary_page
   end
+  
+  private
+  def summary_page
+    @summary_page ||= @agent.click( @homepage.links.text(/Checked Out/) )
+  end
+  
   
 end
--- spec/models/cpl_crawler_spec.rb
+++ spec/models/cpl_crawler_spec.rb
@@ -2,5 +2,9 @@ require 'spec_helper'
 
 describe CplCrawler do
   
+  it "should be able to login into the CPL website" do
+    c = CplCrawler.new('d054635158', '60626')
+    c.login_to_library.content.should include('My Library Card', 'My Personal Information', 'My Preferred Library')
+  end
   
 end
